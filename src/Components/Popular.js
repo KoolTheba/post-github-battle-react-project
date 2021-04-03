b1cce31c96af257b4d574c3680e912decf0ee551
KoolTheba <gomez.teba@gmail.com>
2021-04-03 19:27:50 +0200

    feat: improved styling

--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -134,7 +134,7 @@ export default class Popular extends React.Component {
                 />
                 {this.isLoading() && <p>Loading...</p>}
 
-                {error && <p>{error}</p>}
+                {error && <p className='center-text error'>{error}</p>}
 
                 {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]}/>}
             </React.Fragment>
