1cf7dca90a9ffc0730163aeef2e684dff5074e40
KoolTheba <gomez.teba@gmail.com>
2021-04-03 16:25:22 +0200

    feat: catch fetching repos

--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -30,7 +30,9 @@ export default class Popular extends React.Component {
 
         this.state = {
             selectedLanguage: 'All',
-            repos: null,
+            repos: {
+
+            },
             error: null
         }
 
@@ -45,26 +47,30 @@ export default class Popular extends React.Component {
     updateLanguage(selectedLanguage){
         this.setState({
             selectedLanguage,
-            error: null,
-            repos: null
+            error: null
         })
 
-        fetchPopularRepos(selectedLanguage)
-            .then(repos => {
-                this.setState({
-                    repos,
-                    error: null
+        if(!this.state.repos[selectedLanguage]){
+            fetchPopularRepos(selectedLanguage)
+                .then(data => {
+                    this.setState(({ repos }) => ({
+                        repos: {
+                            ...repos,
+                            [selectedLanguage]: data
+                        }
+                    }))
                 })
-            })
-            .catch(() => {
-                this.setState({
-                    error: `There was an error fetching the repositories of ${selectedLanguage}`
+                .catch(() => {
+                    this.setState({
+                        error: `There was an error fetching the repositories of ${selectedLanguage}`
+                    })
                 })
-            })
+        }
     }
 
     isLoading(){
-        return this.state.repos === null && this.state.error === null
+        const { selectedLanguage, repos, error } = this.state
+        return !repos[selectedLanguage] && error === null
     }
 
     render(){
@@ -80,7 +86,7 @@ export default class Popular extends React.Component {
 
                 {error && <p>{error}</p>}
 
-                {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+                {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
             </React.Fragment>
         )
     }
