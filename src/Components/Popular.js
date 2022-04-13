6dc459448f6b3216ce80f66a759ace41ba60c11f
KoolTheba <gomez.teba@gmail.com>
2022-04-13 18:34:12 +0200

    feat: created High Order Component for Tooltip

--- src/Components/Popular.js
+++ src/Components/Popular.js
@@ -6,6 +6,7 @@ import { fetchPopularRepos } from '../utils/api'
 import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
 import Card from './Card'
 import Loading from './Loading'
+import Tooltip from './Tooltip'
 function LanguagesNav ({selected, onUpdateLanguage}){
     const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python']
     return (
@@ -46,10 +47,12 @@ function ReposGrid ({ repos }){
                         >
                         <ul className='card-list'>
                             <li>
-                                <FaUser color='rgb(255, 191, 116)' size={22} />
-                                <a href={`https://github.com/${login}`}>
-                                {login}
-                                </a>
+                                <Tooltip text="Github username">
+                                    <FaUser color='rgb(255, 191, 116)' size={22} />
+                                    <a href={`https://github.com/${login}`}>
+                                    </a>
+                                    {login}
+                                </Tooltip>
                             </li>
                             <li>
                                 <FaStar color='rgb(255, 215, 0)' size={22} />
