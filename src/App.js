11bd483fddb55eae169d8a3a80b9dbfab189646c
KoolTheba <gomez.teba@gmail.com>
2022-06-17 15:53:24 +0200

    feat: added react lazy loading and Loading component as fallback

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 
 import { ThemeProvider } from './contexts/theme'
 
-import Popular from './components/Popular'
-import Battle from './components/Battle'
+import Loading from './components/Loading'
 import Nav from './components/Nav'
-import Result from './components/Results'
-
 import './index.css'
+
+const Popular = React.lazy(() => import('./components/Popular'))
+const Battle = React.lazy(() => import('./components/Battle'))
+const Result = React.lazy(() => import('./components/Results'))
+
 class App extends React.Component {
   state = {
     theme: 'light',
@@ -30,12 +32,14 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className='container'>
               <Nav />
-              <Switch>
-                <Route exact path={'/'} component={Popular} />
-                <Route exact path={'/battle'} component={Battle} />
-                <Route path={'/battle/results'} component={Result} />
-                <Route render={() => <h1>404</h1>}/>
-              </Switch>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path={'/'} component={Popular} />
+                  <Route exact path={'/battle'} component={Battle} />
+                  <Route path={'/battle/results'} component={Result} />
+                  <Route render={() => <h1>404</h1>}/>
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
